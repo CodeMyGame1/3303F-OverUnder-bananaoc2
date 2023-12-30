d5a4ee4d11d121ab894f3adb808935a17fe41b72
Anjali Sharma-Tiwari <anjalisharmatiwari1@gmail.com>
2023-12-30 00:57:30 -0800

    remove pragma in wrong line

--- include/intake.hpp
+++ include/intake.hpp
@@ -2,5 +2,22 @@
 
 #include "main.h"
 // #include "robot.hpp"
+#pragma once
 
+class Intake {
+    public:
+        pros::Motor_Group intake_motors;
+
+        pros::motor_brake_mode_e brake_mode;
+        
+        Intake(std::vector<pros::Motor> motor_ports, pros::motor_brake_mode_e brake_mode);
+
+        void intake_the_award();
+
+        // :((
+        void outtake_the_award();
+
+        // uh oh
+        void break_the_award();
+};
 void intake_the_award();
